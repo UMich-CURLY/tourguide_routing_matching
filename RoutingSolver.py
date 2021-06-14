Fix a numerical issue.

RoutingSolver.py
@@ -5,7 +5,7 @@ from gurobipy import GRB
 
 class RoutingSolver:
     def __init__(self, veh_num, node_num, human_num, demand_penalty, time_penalty, flag_solver_type):
-        self.LARGETIME = 1000000.0
+        self.LARGETIME = 1000.0
         self.veh_num = veh_num
         self.node_num = node_num
         self.human_num = human_num
@@ -131,6 +131,13 @@ class RoutingSolver:
                 route.append(next_node)
                 curr_node = next_node
             route_list.append(route)
+
+        # for k in [1]:
+        #     for i in range(self.node_num):
+        #         print(self.x_var[k,i,5].varName, self.x_var[k,i,5].x)
+        #     for i in range(self.node_num):
+        #         print(self.x_var[k,i,2].varName, self.x_var[k,i,2].x)
+
         team_list = []
         for i in range(self.node_num-2):
             team = []
@@ -139,5 +146,11 @@ class RoutingSolver:
                     team.append(k)
             team_list.append(team)
             print(team)
+        
+        time_mat = np.zeros((self.veh_num, self.node_num), dtype=np.float64)
+        for k in range(self.veh_num):
+            for i in range(self.node_num):
+                time_mat[k,i] = self.time_var[k,i].x
+        # print(time_mat)
         return route_list, team_list
 

main.py
@@ -4,25 +4,25 @@ from RoutingSolver import RoutingSolver
 from ResultVisualizer import ResultVisualizer
 
 veh_num = 4
-node_num = 8
+node_num = 10
 human_num = 10
 demand_penalty = 10.0
 time_penalty = 1.0
 flag_solver_type = 0
 
 
-node_pose = np.array( [[ 9.84212669 ,  1.80265101],
- [13.03963515 , 16.09854408],
- [19.30844887 , 10.85317018],
- [14.08816635 ,  8.47962349],
- [11.54766554 ,  1.88200905],
- [18.546858   , 14.70094731],
- [10.         , 10.        ],
- [10.         , 10.        ]])
+# node_pose = np.array( [[ 9.84212669 ,  1.80265101],
+#  [13.03963515 , 16.09854408],
+#  [19.30844887 , 10.85317018],
+#  [14.08816635 ,  8.47962349],
+#  [11.54766554 ,  1.88200905],
+#  [18.546858   , 14.70094731],
+#  [10.         , 10.        ],
+#  [10.         , 10.        ]])
 
-# node_pose = np.random.rand(node_num, 2) * 20.0
-# node_pose[-1, :] = 10.0
-# node_pose[-2, :] = 10.0
+node_pose = np.random.rand(node_num, 2) * 20.0
+node_pose[-1, :] = 10.0
+node_pose[-2, :] = 10.0
 
 print('node_pose = ', node_pose)
 