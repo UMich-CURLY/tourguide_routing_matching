b8784ac1074288f3b9607497b1b93bccf64c3a0a
Bo Fu <bofu@umich.edu>
2021-06-14 22:32:06 -0400

    Complete and test the OrtoolHumanMatcher.py.

---
+++ OrtoolHumanMatcher.py
@@ -0,0 +1,88 @@
+import numpy as np
+from ortools.linear_solver import pywraplp
+# Reference: https://developers.google.com/optimization/introduction/python
+
+class OrtoolHumanMatcher:
+    def __init__(self, human_num, veh_num, max_human_in_team):
+        '''
+        max_human_in_team: (veh_num, )
+        '''
+        self.solver = pywraplp.Solver.CreateSolver('GLOP')
+        self.objective = self.solver.Objective()
+        self.z_var = []
+        self.team_constraint = []
+        self.human_constraint = []
+        self.human_num = human_num
+        self.veh_num = veh_num
+        self.max_human_in_team = max_human_in_team + 0
+        # Initialize variables
+        for i_human in range(human_num):
+            for i_veh in range(veh_num):
+                temp_str = 'z_var[' + str(i_human) + ',' + str(i_veh) + ']'
+                temp_x = self.solver.NumVar(0, 1, temp_str)
+                self.z_var.append(temp_x)
+        # Initialize objective function
+        self.objective.SetMinimization()
+        for i_human in range(human_num):
+            for i_veh in range(veh_num):
+                i_z = veh_num * i_human + i_veh
+                self.objective.SetCoefficient(self.z_var[i_z], 1)
+        # Initialize constraints
+        for i_veh in range(veh_num):
+            temp_str = 'veh[' + str(i_veh) + ']'
+            temp_constraint = self.solver.Constraint(0, max_human_in_team[i_veh] + 0.0, temp_str) # Human number in a team
+            for i_human in range(human_num):
+                i_z = veh_num * i_human + i_veh
+                temp_constraint.SetCoefficient(self.z_var[i_z], 1)
+            self.team_constraint.append(temp_constraint)
+
+        for i_human in range(human_num):
+            temp_str = 'human[' + str(i_human) + ']'
+            temp_constraint = self.solver.Constraint(1, 1, temp_str)
+            for i_veh in range(veh_num):
+                i_z = veh_num * i_human + i_veh
+                temp_constraint.SetCoefficient(self.z_var[i_z], 1)
+            self.human_constraint.append(temp_constraint)
+
+    def optimize(self, human_demand_bool, y_sol):
+        '''
+        y_sol: (veh_num, place_num)
+        scores: (human_num, veh_num)
+        '''
+        veh_num, place_num = y_sol.shape
+        reverse_y_sol = 1.0 - y_sol
+        assert veh_num == self.veh_num, 'OrtoolHumanMatcher.optimize: assertation error!'
+        prior_offset = 0.0
+        # Update the objective function
+        scores = np.zeros((self.human_num, self.veh_num), dtype=np.float64)
+        for i_human in range(self.human_num):
+            for i_veh in range(self.veh_num):
+                scores[i_human, i_veh] = (human_demand_bool[i_human] * reverse_y_sol[i_veh]).sum() - prior_offset * i_veh
+                i_z = self.veh_num * i_human + i_veh
+                self.objective.SetCoefficient(self.z_var[i_z], scores[i_human, i_veh])
+        # Optimize
+        flag_solver = self.solver.Solve()
+        assert flag_solver == self.solver.OPTIMAL or flag_solver == self.solver.FEASIBLE, 'MatchLinProg: infeasible'
+        # Get solution
+        human_in_team = np.empty((self.human_num), dtype=int)
+        z_sol = np.zeros((self.human_num, self.veh_num), dtype=np.float64)
+        for i_human in range(self.human_num):
+            for i_veh in range(self.veh_num):
+                i_z = self.veh_num * i_human + i_veh
+                temp_z_sol = self.z_var[i_z].solution_value()
+                z_sol[i_human, i_veh] = temp_z_sol
+                if temp_z_sol > 0.5:
+                    human_in_team[i_human] = i_veh
+                    break
+        # human_in_team_raw = scores.argmin(axis=1)
+        # print('human_in_team_raw = ', human_in_team_raw)
+        # print('human_in_team_diff = ', human_in_team - human_in_team_raw)
+        best_match_score = scores.min(axis=1)
+        curr_match_score = (scores * z_sol).sum(axis=1)
+        # print('best_match_score = ', best_match_score)
+        # print('curr_match_score = ', curr_match_score)
+        total_demand = human_demand_bool.sum()
+        dropped_demand = curr_match_score.sum()
+        min_dropped_demand = best_match_score.sum()
+        demand_result = [total_demand, dropped_demand, min_dropped_demand]
+        return human_in_team, z_sol, demand_result
--- match_main.py
+++ match_main.py
@@ -4,68 +4,58 @@ from GurobiRoutingSolver import GurobiRoutingSolver
 from OrtoolRoutingSolver import OrtoolRoutingSolver
 from ResultVisualizer import ResultVisualizer
 from ResultEvaluator import ResultEvaluator
+from OrtoolHumanMatcher import OrtoolHumanMatcher
 
 veh_num = 4
 node_num = 10
-human_num = 10
 demand_penalty = 10.0
 time_penalty = 1.0
 flag_solver_type = 0
 
+human_num = 10
+human_choice = 5
 
-# node_pose = np.array( [[ 9.84212669 ,  1.80265101],
-#  [13.03963515 , 16.09854408],
-#  [19.30844887 , 10.85317018],
-#  [14.08816635 ,  8.47962349],
-#  [11.54766554 ,  1.88200905],
-#  [18.546858   , 14.70094731],
-#  [10.         , 10.        ],
-#  [10.         , 10.        ]])
+max_human_in_team = np.ones(veh_num, dtype=int) * (human_num // veh_num + 1)
+place_num = node_num - 2
 
-# node_pose = np.array( [[ 17.87230637 , 35.1245531 ],
-#  [ 84.76256192 ,198.77716023],
-#  [151.94962027 , 78.23502452],
-#  [ 25.62683298 ,154.40066714],
-#  [ 43.72012928 ,161.59792387],
-#  [189.77192707 ,169.84716883],
-#  [194.84484267 ,173.51820618],
-#  [160.98953757 , 58.55608649],
-#  [100.         ,100.        ],
-#  [100.         ,100.        ]])
-
+# Initialize spacial maps
 node_pose = np.random.rand(node_num, 2) * 200.0
 node_pose[-1, :] = 100.0
 node_pose[-2, :] = 100.0
-
-print('node_pose = ', node_pose)
 
 edge_dist = squareform(pdist(node_pose))
 veh_speed = np.ones(veh_num, dtype=np.float64)
 
 edge_time = edge_dist.reshape(1,node_num,node_num) / veh_speed.reshape(veh_num,1,1)
 node_time = np.ones((veh_num,node_num), dtype=np.float64) * 30.0
+print('node_pose = ', node_pose)
 
-# print(edge_time, node_time)
+# Initialize human selections
+human_demand_int = np.random.randint(0, place_num, (human_num,human_choice) )
+temp_index = np.arange(human_num).reshape(-1,1).repeat(human_choice,axis=1)
+human_demand_bool = np.zeros((human_num, place_num), dtype=np.float64)
+human_demand_bool[temp_index.reshape(-1), human_demand_int.reshape(-1)] = 1.0
+human_demand_int_unique = []
+for l in range(human_num):
+    human_demand_int_unique.append(np.unique(human_demand_int[l]))
+print('human_demand_int_unique = \n', human_demand_int_unique)
 
+# Initialize the visualizer and evaluator
 visualizer = ResultVisualizer()
 evaluator = ResultEvaluator(veh_num, node_num, human_num, demand_penalty, time_penalty)
 
-routing_solver = GurobiRoutingSolver(veh_num, node_num, human_num, demand_penalty, time_penalty, flag_solver_type)
-routing_solver.set_model(edge_time, node_time)
-routing_solver.set_all_task_complete()
-routing_solver.set_objective()
-routing_solver.optimize()
-route_list, route_time_list, team_list, y_sol = routing_solver.get_plan()
-visualizer.print_results(route_list, route_time_list, team_list)
-visualizer.visualize_routes(node_pose, route_list)
-result_max_time, node_visit = evaluator.objective_fcn(edge_time, node_time, route_list, team_list)
-print('result_max_time = ', result_max_time)
-print('node_visit = ', node_visit)
 
 routing_solver = OrtoolRoutingSolver(veh_num, node_num, human_num, demand_penalty, time_penalty, flag_solver_type)
 routing_solver.set_model(edge_time, node_time)
 routing_solver.optimize()
 route_list, route_time_list, team_list, y_sol = routing_solver.get_plan()
+
+matcher = OrtoolHumanMatcher(human_num, veh_num, max_human_in_team)
+human_in_team, z_sol, demand_result = matcher.optimize(human_demand_bool, y_sol)
+print('human_in_team', human_in_team)
+print('z_sol', z_sol)
+print('demand_result = ', demand_result)
+
 
 visualizer.print_results(route_list, route_time_list, team_list)
 visualizer.visualize_routes(node_pose, route_list)
